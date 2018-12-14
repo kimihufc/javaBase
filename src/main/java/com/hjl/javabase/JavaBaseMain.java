c1d803438b60e428dc91d2fb285e8ec56729a94a
hujianglai <hujianglai@yunzhangfang.com>
2018-12-14 09:28:03 +0800

    function

---
+++ src/main/java/com/hjl/javabase/JavaBaseMain.java
@@ -0,0 +1,9 @@
+package com.hjl.javabase;
+
+/**
+ * @author: HJL
+ * @create: 2018-12-13 20:27
+ */
+public class JavaBaseMain {
+
+}
---
+++ src/main/java/com/hjl/javabase/function/FunctionDemo.java
@@ -0,0 +1,20 @@
+package com.hjl.javabase.function;
+
+import java.util.function.Function;
+
+/**
+ * @author: HJL
+ * @create: 2018-12-13 20:27
+ */
+public class FunctionDemo {
+
+    public static void main(String[] args){
+        Function<String,Object> func = str -> str+"123";
+        System.out.println(func.apply("123"));
+        Function<Integer,Integer> doubleVal = e->e*2;
+        Function<Integer,Integer> squreVal = e->e*e;
+        System.out.println(doubleVal.andThen(squreVal).apply(3));
+        System.out.println(doubleVal.compose(squreVal).apply(3));
+    }
+
+}
