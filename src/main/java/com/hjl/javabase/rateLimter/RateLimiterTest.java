f3cd8fa8526082f9f828433b85ffb9840b1825a7
hujianglai <hujianglai@yunzhangfang.com>
2018-12-15 17:40:46 +0800

    ratelimiter

---
+++ src/main/java/com/hjl/javabase/rateLimter/RateLimiterTest.java
@@ -0,0 +1,41 @@
+package com.hjl.javabase.rateLimter;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author: HJL
+ * @create: 2018-12-15 17:36
+ */
+public class RateLimiterTest {
+    public static void main(String[] args) {
+        //0.5代表一秒最多多少个
+        RateLimiter rateLimiter = RateLimiter.create(0.5);
+        List<Runnable> tasks = new ArrayList<Runnable>();
+        for (int i = 0; i < 10; i++) {
+            tasks.add(new UserRequest(i));
+        }
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        for (Runnable runnable : tasks) {
+            System.out.println("等待时间：" + rateLimiter.acquire());
+            threadPool.execute(runnable);
+        }
+    }
+
+    private static class UserRequest implements Runnable {
+        private int id;
+
+        public UserRequest(int id) {
+            this.id = id;
+        }
+
+        public void run() {
+            System.out.println(id);
+        }
+    }
+
+}
